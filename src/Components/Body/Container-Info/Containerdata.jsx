7fa5b407c4676db4acf029985d65f8d4e74ca954
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-10-12 11:22:43 +0100

    updated the body section icons

--- src/Components/Body/Container-Info/Containerdata.jsx
+++ src/Components/Body/Container-Info/Containerdata.jsx
@@ -1,7 +1,7 @@
 const dataSet1 = [
 	{
-		// img1: "src/assets/Iconss/pen.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/pen.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Blog writng",
@@ -11,8 +11,8 @@ const dataSet1 = [
 		classname_img2: "img-prop2",
 	},
 	{
-		// img1: "src/assets/Iconss/basil.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/basil.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Product description",
@@ -22,8 +22,8 @@ const dataSet1 = [
 	},
 
 	{
-		// img1: "src/assets/Iconss/text.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/text.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Article writer",
@@ -34,8 +34,8 @@ const dataSet1 = [
 	},
 
 	{
-		// img1: "src/assets/Iconss/pen.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/pen.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Blog writng",
@@ -48,8 +48,8 @@ const dataSet1 = [
 
 export const dataSet2 = [
 	{
-		// img1: "src/assets/Iconss/pen.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/pen.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Blog writng",
@@ -59,8 +59,8 @@ export const dataSet2 = [
 		classname_img2: "img-prop2",
 	},
 	{
-		// img1: "src/assets/Iconss/basil.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/basil.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Product description",
@@ -70,8 +70,8 @@ export const dataSet2 = [
 	},
 
 	{
-		// img1: "src/assets/Iconss/text.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/text.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Article writer",
@@ -81,8 +81,8 @@ export const dataSet2 = [
 		classname_img2: "img-prop2",
 	},
 	{
-		// img1: "src/assets/Iconss/pen.svg",
-		// img2: "src/assets/Iconss/arrow.svg",
+		img1: "src/assets/Iconss/pen.svg",
+		img2: "src/assets/Iconss/arrow.svg",
 		alt1: "image1",
 		alt2: "image2",
 		title: "Blog writng",
