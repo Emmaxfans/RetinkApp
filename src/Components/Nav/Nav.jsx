f0384c89ece8371203a34621d663e77b76dc1cd4
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-10-12 07:49:15 +0100

    updated the top nav section

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,5 +1,10 @@
 import "./Nav.css";
 import alert from "../../assets/Icons/alert.svg";
+import search from "../../assets/Icons/search.svg";
+import c20 from "../../assets/Icons/c20.svg";
+import calender from "../../assets/Icons/calender.svg";
+import profile from "../../assets/Icons/profile.svg";
+
 import Button from "../Button/Button";
 
 const Nav = () => {
@@ -9,9 +14,12 @@ const Nav = () => {
 				<div className="nav-div1">
 					<div className="nav-sub1">
 						<div className="sub1">
+							<img src={search} alt="search-button" />
 							<p>Search for templates, projects, etc</p>
 						</div>
 					</div>
+
+					<div className="nav-space"></div>
 
 					<div className="nav-sub2">
 						<button>start</button>
@@ -19,14 +27,14 @@ const Nav = () => {
 					</div>
 
 					<div className="nav-sub3">
-						<img src={alert} alt="img1" />
+						<img src={c20} alt="c20" />
 					</div>
 				</div>
 
 				<div className="nav-div2">
+					<img src={calender} alt="img1" />
 					<img src={alert} alt="img1" />
-					<img src={alert} alt="img1" />
-					<img src={alert} alt="img1" />
+					<img src={profile} alt="profile-img" />
 				</div>
 			</div>
 		</>
