be141378fb57fc01e3990a4d85558dd0d302e66e
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-10-12 11:21:00 +0100

    updated the body section icons

--- src/Components/Body/Container-Info/ContainerCard.jsx
+++ src/Components/Body/Container-Info/ContainerCard.jsx
@@ -3,6 +3,103 @@ import "./ContainerCard.css";
 import dataSet1, { dataSet2 } from "./Containerdata";
 
 const ContainerCard = () => {
+	const dataSet1 = [
+		{
+			img1: "src/assets/Iconss/pen.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Blog writng",
+			description:
+				"Generate the best blog post to fit your audience with just few clicks of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+		{
+			img1: "src/assets/Iconss/basil.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Product description",
+			description: "Instantly generate engaging product descriptions that sell",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+
+		{
+			img1: "src/assets/Iconss/text.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Article writer",
+			description:
+				"Automatically create unique factual articles at the touch of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+
+		{
+			img1: "src/assets/Iconss/pen.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Blog writng",
+			description:
+				"Generate the best blog post to fit your audience with just few clicks of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+	];
+
+	const dataSet2 = [
+		{
+			img1: "src/assets/Iconss/pen.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Blog writng",
+			description:
+				"Generate the best blog post to fit your audience with just few clicks of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+		{
+			img1: "src/assets/Iconss/basil.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Product description",
+			description: "Instantly generate engaging product descriptions that sell",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+
+		{
+			img1: "src/assets/Iconss/text.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Article writer",
+			description:
+				"Automatically create unique factual articles at the touch of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+		{
+			img1: "src/assets/Iconss/pen.svg",
+			img2: "src/assets/Iconss/arrow.svg",
+			alt1: "image1",
+			alt2: "image2",
+			title: "Blog writng",
+			description:
+				"Generate the best blog post to fit your audience with just few clicks of a button",
+			classname_img1: "img-prop1",
+			classname_img2: "img-prop2",
+		},
+	];
+
+	// export default dataSet1;
+
 	return (
 		<div className="container-card">
 			<h2>Social Media</h2>
